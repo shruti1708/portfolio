d6a13af1d2f43f20198beba3ebd121d08fa06ad9
shruti1708 <63550530+shruti1708@users.noreply.github.com>
2026-04-07 18:05:47 +0530

    Update Home2.js

--- Portfolio-master/src/components/Home/Home2.js
+++ Portfolio-master/src/components/Home/Home2.js
@@ -4,7 +4,6 @@ import myImg from "../../Assets/shruti.jpg";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
-  AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
