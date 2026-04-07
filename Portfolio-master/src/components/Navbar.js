3f2cebeb01241f87766cbcdcfb5e4f14a37bd809
shruti1708 <63550530+shruti1708@users.noreply.github.com>
2026-04-07 18:15:42 +0530

    Update Navbar.js

--- Portfolio-master/src/components/Navbar.js
+++ Portfolio-master/src/components/Navbar.js
@@ -6,7 +6,7 @@ import logo from "../Assets/logo.png";
 import { Link } from "react-router-dom";
 import {
   AiOutlineHome,
-  AiOutlineFundProjectionScreen,
+  
   AiOutlineUser,
 } from "react-icons/ai";
 
